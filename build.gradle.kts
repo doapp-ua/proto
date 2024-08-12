da778ea68bc96fba611fd129eb4dc324dd9750af
Nan1t <imnanit@gmail.com>
2024-08-12 14:18:57 +0300

    Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,18 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.serialization) apply false
+    `maven-publish`
 }
+
+//afterEvaluate {
+//    publishing {
+//        publications {
+//            create<MavenPublication>("maven") {
+//                from (components["proto"])
+//                groupId = "com.doapp.proto"
+//                artifactId = "proto"
+//                version = "1.0"
+//            }
+//        }
+//    }
+//}
